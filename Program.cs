36ea6ab35a97f9c6acf7f31af69d3d18109507b8
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-14 03:34:04 -0700

    namespace

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using addElements;
+using data;
+using subject;
+using System.Formats.Asn1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        student chair = new student();
+        chair.work();
+
+
+        database dat = new database();
+        dat.order();
+
+        addition area1 = new addition();
+        area1.areaof();
+        
+    }
+}
+
+namespace addElements
+{
+    class addition
+    {
+        int area = 0;
+        public int areaof(int lenght, int wieght)
+        {
+            area = lenght * wieght;
+            return area;
+        }
+    }
+}
